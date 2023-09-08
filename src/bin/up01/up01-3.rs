86421f4de66da1f3a8470d6739482ddb9b9f4603
elkcl <28754537+elkcl@users.noreply.github.com>
2023-09-09 00:21:12 +0300

    up01-3 in progress

--- src/bin/up01/up01-3.rs
+++ src/bin/up01/up01-3.rs
@@ -1,5 +1,13 @@
 use std::io;
 
+fn divmod(c: i32, a: i32, n: i32) -> i32 {
+    let mut x: i32;
+    let mut y: i32;
+}
+
 fn main() {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Input error");
+    let n: i32 = n.trim().parse().expect("Input format error");
 
 }
