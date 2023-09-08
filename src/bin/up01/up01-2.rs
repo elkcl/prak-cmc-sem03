39b77493434927ce7761ed4418e061f7a60b8393
Elk Cloner <dev@elkcl.ru>
2023-09-08 18:33:51 +0300

    up01-2 done

--- src/bin/up01/up01-2.rs
+++ src/bin/up01/up01-2.rs
@@ -1,25 +1,54 @@
-use std::{io, slice};
-use std::io::Read;
+use std::{io};
+use std::io::{Read, Write};
 
-const XOR_MASK: u8 = 1u8 << 3;
-const AND_MASK: u8 = !(1u8 << 2);
-const CODE_AT: u8 = 0;
-const CODE_DIGITS: u8 = 1;
-const CODE_LOWERCASE: u8 = 11;
-const CODE_UPPERCASE: u8 = 37;
-const CODE_SHARP: u8 = 63;
+const XOR_MASK: i32 = 1 << 3;
+const AND_MASK: i32 = !(1 << 2);
+const CODE_AT: i32 = 0;
+const CODE_DIGITS: i32 = 1;
+const CODE_LOWERCASE: i32 = 11;
+const CODE_UPPERCASE: i32 = 37;
+const CODE_SHARP: i32 = 63;
+
+fn getchar(h: &mut impl Read) -> Option<u8> {
+    let mut buf: [u8; 1] = [b'\0'];
+    if h.read(&mut buf).expect("Input error") > 0 {
+        Some(buf[0])
+    } else {
+        None
+    }
+}
+
+fn putchar(h: &mut impl Write, c: u8) -> () {
+    let buf: [u8; 1] = [c];
+    h.write(&buf).expect("Output error");
+}
 
 fn main() {
-    let stdin = io::stdin();
-    let mut c: u8 = b'\0';
-    while stdin
-        .lock()
-        .read(slice::from_mut(&mut c))
-        .expect("Input error")
-        > 0 {
-        let mut res: u8 = c;
+    let mut stdin = io::stdin().lock();
+    let mut stdout = io::stdout().lock();
+    while let Some(c) = getchar(&mut stdin) {
+        let mut res: i32 = c as i32;
         if b'0' <= c && c <= b'9' {
-
+            res += CODE_DIGITS - b'0' as i32;
+        } else if b'a' <= c && c <= b'z' {
+            res += CODE_LOWERCASE - b'a' as i32;
+        } else if b'A' <= c && c <= b'Z' {
+            res += CODE_UPPERCASE - b'A' as i32;
+        } else {
+            continue;
+        }
+        res ^= XOR_MASK;
+        res &= AND_MASK;
+        if res == CODE_AT {
+            putchar(&mut stdout, b'@');
+        } else if res == CODE_SHARP {
+            putchar(&mut stdout, b'#');
+        } else if res < CODE_LOWERCASE {
+            putchar(&mut stdout, (res - CODE_DIGITS + b'0' as i32) as u8);
+        } else if res < CODE_UPPERCASE {
+            putchar(&mut stdout, (res - CODE_LOWERCASE + b'a' as i32) as u8);
+        } else {
+            putchar(&mut stdout, (res - CODE_UPPERCASE + b'A' as i32) as u8);
         }
     }
 }
